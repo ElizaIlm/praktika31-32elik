9f1f957e4af81556aee0735474d56f190edc85a8
ilmasheva-eliza <ilmeliza36@gmai.com>
2026-04-16 16:22:03 +0500

    создала модели представления

---
+++ Common2/ViewModelGames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common2
+{
+    public class ViewModelGames
+    {
+        public Snakes SnakesPlayers { get; set; }
+        public Snakes.Point Points { get; set; }
+        public int Top = 0;
+        public int IdSnake { get; set; }
+    }
+}
---
+++ Common2/ViewModelUserSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common2
+{
+    public class ViewModelUserSettings
+    {
+        public string IPAddress { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        public int IdSnake = -1;
+    }
+}
